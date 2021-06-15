25318cd1d4b0d76d57927b99cc0f128cb94cb43b
Vladislav <kelsenelenelvial@mail.ru>
2021-06-15 22:51:53 +0300

    Start portfolio

---
+++ src/js/main.js
@@ -0,0 +1,12 @@
+/**
+  * название функции
+  *
+  * //@param {number} first - первое число
+  * //@returns {number}
+  */
+
+import greeting from "./components/greeting.js";
+
+greeting();
+greeting();
+
