e609c4749427618f80099773034436727f759bd6
Vladislav <kelsenelenelvial@mail.ru>
2021-06-25 21:40:06 +0300

    preloader info

--- src/js/main.js
+++ src/js/main.js
@@ -8,7 +8,27 @@
 import { headAnimation, headText, headerSticky, burger } from "./components/headAnim.js";
 import workCards from "./components/works.js";
 
+const startPreloader = () => {
+	//Обработка стартовой анимации
+	const preloader = document.querySelector(".preloader");
+
+	preloader.classList.add("preloader-animation");
+
+	setTimeout(() => {
+		preloader.classList.remove("preloader-animation");
+
+		//Скрываем прелодер
+		preloader.classList.add("preloader-hidden");
+	}, 3000);
+
+	setTimeout(() => {
+		//Убираем прелодер со страницы
+		preloader.classList.add("preloader-none");
+	}, 3200);
+};
+
 window.addEventListener("DOMContentLoaded", () => {
+	startPreloader();
 	headAnimation();
 	headText();
 	headerSticky();
