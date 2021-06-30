297769a4af9ff97c02cea10f358a585fa0d3b421
Vladislav <kelsenelenelvial@mail.ru>
2021-06-30 16:17:59 +0300

    cards content

--- src/js/components/works.js
+++ src/js/components/works.js
@@ -1,5 +1,45 @@
 "use strict";
 
+const createCards = (works) => {
+	const { id, img, title, description, revInfo, sourceLink, siteLink } = works;
+	const cardsWrap = document.querySelector(".card-wrap");//Обертка
+	const card = document.createElement("div");
+	card.classList.add("card");
+	card.setAttribute("data-rev", `${id}`);
+
+	card.insertAdjacentHTML("beforeend", `
+		<img src="${img}" alt="photo">
+		<div class="card-body">
+			<div class="card-title">${title}</div>
+			<div class="card-info">${description}</div>
+		</div>
+		<div class="card-more">
+			<button type="button" class="card-more-descr">Подробнее</button>
+		</div>
+
+
+		<div class="card-reveal">
+			<div class="close">&times;</div>
+			<div class="card-reveal-body">
+				<div class="card-reveal-text">
+					${revInfo}
+				</div>
+				<div class="card-reveal-links">
+					<div class="source reveal-item">
+						<a href="${sourceLink}" target="_blank"><img src="../img/code.png"
+								alt="code" title="Посмотреть код"></a>
+					</div>
+					<div class="site reveal-item">
+						<a href="${siteLink}" target="_blank"><img src="../img/site.png" alt="website" title="Перейти на сайт"></a>
+					</div>
+				</div>
+			</div>
+		</div>
+	`);
+	cardsWrap.appendChild(card);
+
+};
+
 const workCards = () => {
 
 	const cards = document.querySelectorAll(".card");
@@ -30,4 +70,5 @@ const workCards = () => {
 
 };
 
-export default workCards;
+
+export { workCards, createCards };
--- src/js/main.js
+++ src/js/main.js
@@ -6,13 +6,15 @@
   */
 
 import { headAnimation, headText, headerSticky, burger } from "./components/headAnim.js";
-import workCards from "./components/works.js";
+import { workCards, createCards } from "./components/works.js";
 
 const startPreloader = () => {
 	//Обработка стартовой анимации
 	const preloader = document.querySelector(".preloader");
 
 	preloader.classList.add("preloader-animation");
+	//Убираем скролл
+	document.body.style.overflow = "hidden";
 
 	setTimeout(() => {
 		preloader.classList.remove("preloader-animation");
@@ -24,6 +26,9 @@ const startPreloader = () => {
 	setTimeout(() => {
 		//Убираем прелодер со страницы
 		preloader.classList.add("preloader-none");
+
+		//Возвращаем скролл
+		document.body.style.overflow = "";
 	}, 3200);
 };
 
@@ -36,49 +41,7 @@ const getData = async function (url) {//Работа с базой данных
 	}
 	return await response.json();
 };
-
-console.log(getData("./database/db.json"));
-
-
-const createCards = (works) => {
-	const { id, img, title, description, revInfo, sourceLink, siteLink } = works;
-	const cardsWrap = document.querySelector(".card-wrap");//Обертка
-	const card = document.createElement("div");
-	card.classList.add("card");
-	card.setAttribute("data-rev", `${id + 4}`);
-
-	card.insertAdjacentHTML("beforeend", `
-		<img src="${img}" alt="photo">
-		<div class="card-body">
-			<div class="card-title">${title}</div>
-			<div class="card-info">${description}</div>
-		</div>
-		<div class="card-more">
-			<button type="button" class="card-more-descr">Подробнее</button>
-		</div>
-
-
-		<div class="card-reveal">
-			<div class="close">&times;</div>
-			<div class="card-reveal-body">
-				<div class="card-reveal-text">
-					${revInfo}
-				</div>
-				<div class="card-reveal-links">
-					<div class="source reveal-item">
-						<a href="${sourceLink}" target="_blank"><img src="../img/code.png"
-								alt="code" title="Посмотреть код"></a>
-					</div>
-					<div class="site reveal-item">
-						<a href="${siteLink}"><img src="../img/site.png" alt="website" title="Перейти на сайт"></a>
-					</div>
-				</div>
-			</div>
-		</div>
-	`);
-	cardsWrap.appendChild(card);
-
-};
+//console.log(getData("./database/db.json"));
 
 const renderCards = () => {
 	getData("./database/db.json")
@@ -97,6 +60,6 @@ window.addEventListener("DOMContentLoaded", () => {
 	headerSticky();
 	burger();
 	renderCards();
-	workCards();
+	//workCards();
 });
 
